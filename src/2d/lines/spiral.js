44cbb9b248296c8f426dedd8b95fae456ccd7be5
amit-ksh <ksamit1110@gmail.com>
2023-12-04 12:46:40 +0530

    spiral

--- src/2d/lines/spiral.js
+++ src/2d/lines/spiral.js
@@ -2,32 +2,35 @@ const canvasSketch = require("canvas-sketch");
 const random = require("canvas-sketch-util/random");
 const palettes = require("nice-color-palettes");
 
+const palette = random.pick(palettes);
+
 const settings = {
-  dimensions: [2048, 2048],
+  // name: "spiral",
+  dimensions: [512, 512],
   animate: true,
-  fps: 60,
 };
 
 const bgColor = "rgb(0, 0, 0)";
-const cellSize = 75;
-const lineWidth = 0.4;
-let radius = 2;
+const cellSize = 10;
+const lineWidth = 1;
+const padding = 20;
+let radius = 0.2;
 const sketch = ({ context, width, height }) => {
   window.document.body.style.background = bgColor;
 
   const gradient = context.createLinearGradient(0, 0, width, height);
-  gradient.addColorStop("0.1", "#ff5c33");
-  gradient.addColorStop("0.2", "#ff66b3");
-  gradient.addColorStop("0.4", "#ccccff");
-  gradient.addColorStop("0.6", "#b3ffff");
-  gradient.addColorStop("0.8", "#80ff80");
-  gradient.addColorStop("0.9", "#ffff33");
+  gradient.addColorStop("0.1", random.pick(palette));
+  gradient.addColorStop("0.2", random.pick(palette));
+  gradient.addColorStop("0.4", random.pick(palette));
+  gradient.addColorStop("0.6", random.pick(palette));
+  gradient.addColorStop("0.8", random.pick(palette));
+  gradient.addColorStop("0.9", random.pick(palette));
 
   context.lineWidth = lineWidth;
 
   let timestamp = 0;
   const interval = 1000 / 60;
-  let vr = 0.009;
+  let vr = 0.01;
 
   return ({ context, width, height, deltaTime }) => {
     if (true || timestamp < interval) {
@@ -35,21 +38,30 @@ const sketch = ({ context, width, height }) => {
       // context.fillStyle = gradient;
       context.fillRect(0, 0, width, height);
 
+      if (radius > 5 || radius < 0) vr *= -1;
       radius += vr;
-      if (radius > 5 || radius < -5) vr *= -1;
 
-      for (let y = 0; y < height; y += cellSize) {
-        for (let x = 0; x < width; x += cellSize) {
+      for (let y = padding; y < height - padding; y += cellSize) {
+        for (let x = padding; x < width - padding; x += cellSize) {
           const angle =
-            (Math.cos((x - 1000) * 0.003) + Math.sin((y - 400) * 0.003)) *
+            (Math.cos((x - width / 2 + 10) * 0.01) +
+              Math.sin((y - height / 2 - 480) * 0.01)) *
             radius;
 
           context.save();
           context.beginPath();
-          // context.strokeStyle = gradient;
-          context.strokeStyle = "white";
+          context.strokeStyle = gradient;
+          // context.strokeStyle = "white";
           context.moveTo(x, y);
           context.lineTo(x + Math.cos(angle) * 50, y + Math.sin(angle) * 50);
+          // context.arc(
+          //   x + Math.tan(angle) * Math.cos(random.noise1D(1)),
+          //   y + Math.tan(angle) * Math.cos(random.noise1D(1)),
+          //   radius,
+          //   random.gaussian(180, 100),
+          //   random.gaussian(180, 300),
+          //   true
+          // );
           context.stroke();
           context.restore();
         }
